3b18550f38caaf18bdbe3f861bf7dc934fd86538
Eneko Sanz <contact@eneko.me>
2020-06-12 20:55:44 +0200

    Starts the implementation of the 'Get in touch' modal.

--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,31 @@
  */
 
 const scrollToAnchor = document.getElementsByClassName('scroll')[0];
+const contactModalElement = document.getElementsByClassName('contact-modal')[0];
 const mainElement = document.getElementsByTagName('main')[0];
+const getInTouchButton = document.querySelectorAll('.get-in-touch > button')[0];
 
 // Avoids scroll restoration on page reload.
 if(history && history.scrollRestoration) {
   history.scrollRestoration = "manual";
 }
 
-scrollToAnchor.addEventListener('click', function(event) {
+scrollToAnchor.addEventListener('click', (event) => {
   mainElement.scrollIntoView({
     behavior: 'smooth',
     block: 'start'
   })
   scrollToAnchor.blur();
   event.preventDefault();
+});
+
+getInTouchButton.addEventListener('click', (event) => {
+  contactModalElement.className = 'contact-modal open';
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === 'Escape' || event.key === 'Esc') {
+    contactModalElement.className = 'contact-modal';
+    event.preventDefault();
+  }
 });
