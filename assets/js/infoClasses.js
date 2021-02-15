9c672f2e0ddc79dabf17bdfebf9422d27ca8458a
Eneko <contact@eneko.me>
2021-02-15 23:45:26 +0100

    Adds suport for ES modules through Rollup and starts the implementation of the new GitHub/Npm info feature.

---
+++ assets/js/infoClasses.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright © 2020 Eneko Sanz <contact@eneko.me>
+ * File distributed under the MIT license.
+ */
+class Info {
+  constructor(container) {
+    this.container = container;
+  }
+
+  async fetch() {
+    const res = await fetch(this.url);
+    this.list = this.transform(await res.json());
+    console.log(this.list);
+  }
+
+  insert() {
+    const figure = document.createElement('figure');
+    const figcaption = document.createElement('figcaption');
+    const ul = document.createElement('ul');
+    figcaption.append(document.createTextNode(this.title));
+    figure.append(figcaption);
+    ul.innerHTML = this.list.reduce((acc, val) => `${acc}<li>${val}</li>`);
+    figure.append(ul);
+    this.container.append(figure);
+  }
+}
+
+export class GitHubInfo extends Info {
+  constructor(container, userName, repoNumber) {
+    super(container);
+    const userReposUrl = `https://api.github.com/users/${userName}/repos`;
+    this.url = `${userReposUrl}?per_page=${repoNumber}&page=1&sort=updated`;
+    this.title = 'LAST GITHUB REPOS';
+  }
+
+  transform(data) {
+    return data.map(item => item.name);
+  }
+}
+
+export class NpmInfo extends Info {
+  constructor(container, userName, pkgNumber) {
+    super(container);
+    const searchUlr = 'https://registry.npmjs.org/-/v1/search';
+    this.url = `${searchUlr}?text=author:${userName}&size=${pkgNumber}`;
+    this.title = 'LAST NPM PACKAGES';
+  }
+
+  transform(data) {
+    return data.objects.map(item => `${item.package.name} v${item.package.version}`);
+  }
+}
